a3c35d1b953c97ad0219994dcee1a503c9c81a50
sabir222 <mrsabir4@gmail.com>
2024-05-01 03:23:53 +0100

    update: update cfg

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,5 +1,7 @@
 return {
   -- save shortcut keymap
-  vim.keymap.set('n', '<C-s>', '<Esc>:w<CR>a', {}),
-  vim.keymap.set('n', '<C-x>', '<Esc>:bd<CR>', {}),
+  vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', {}),
+  vim.keymap.set({ 'n', 'i' }, '<C-f>', ':copy.<CR>', {}),
+  vim.keymap.set({ 'n', 'i' }, '<C-x>', '<Esc>:bd<CR>', {}),
+  vim.keymap.set('n', '<tab>', '<Esc>:bnext<CR>', {}),
 }
