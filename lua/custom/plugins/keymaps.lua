2c845f112785c6c34d1635b012fa29bb232ea40e
sabir222 <mrsabir4@gmail.com>
2024-05-02 04:27:53 +0100

    update:updating cfg

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -2,6 +2,6 @@ return {
   -- save shortcut keymap
   vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-f>', ':copy.<CR>', {}),
-  vim.keymap.set({ 'n', 'i' }, '<C-x>', '<Esc>:bd<CR>', {}),
-  vim.keymap.set('n', '<Tab>', '<Esc>:bnext<CR>', {}),
+  vim.keymap.set({ 'n', 'i' }, '<C-x', '<Esc>:bd<CR>', {}),
+  vim.keymap.set('n', '<C-\\>', '<Esc>:bnext<CR>', {}), -- use tabedit to use buffer plugin
 }
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,9 +15,18 @@ return {
   },
   opts = {
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          --"node_modules"
+        },
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
         },
       },
     },
