55e6125dcebfeefeca3bbac3dcc0e6619481ec69
sabir222 <mrsabir4@gmail.com>
2024-05-02 22:05:51 +0100

    update: adding harpoon

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,7 +1,6 @@
 return {
-  -- save shortcut keymap
+  vim.keymap.set('n', '<;>', ':Neotree toggle<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-f>', ':copy.<CR>', {}),
-  vim.keymap.set({ 'n', 'i' }, '<C-x', '<Esc>:bd<CR>', {}),
-  vim.keymap.set('n', '<C-\\>', '<Esc>:bnext<CR>', {}), -- use tabedit to use buffer plugin
+  vim.keymap.set({ 'n', 'i' }, '<C-x>', '<Esc>:bd<CR>', {}),
 }
