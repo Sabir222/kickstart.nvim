ecaaf71797934e6677fbcfbd302331010a4c4c23
sabir222 <mrsabir4@gmail.com>
2024-05-12 15:24:02 +0100

    update : updating cfg

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -3,4 +3,5 @@ return {
   vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-f>', ':copy.<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-x>', '<Esc>:bd<CR>', {}),
+  vim.keymap.set({ 'n', 'i' }, '<C-z>', '', {}),
 }
