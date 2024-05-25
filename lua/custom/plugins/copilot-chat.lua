7ad204c7fc09c9b895b306892200a38c6512afe0
sabir222 <mrsabir4@gmail.com>
2024-05-25 17:46:41 +0100

    feature: adding copilot chat

---
+++ lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      question_header = '## sabir', -- Header to use for user questions
+      answer_header = '## Copilot ', -- Header to use for AI answers
+      error_header = '## Error ', -- Header to use for errors
+      separator = '---', -- Separator to use in chat
+      model = 'gpt-4', -- GPT model to use, 'gpt-3.5-turbo' or 'gpt-4'
+      temperature = 0.1, -- GPT temperature
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -3,5 +3,12 @@ return {
   vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-f>', ':copy.<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-x>', '<Esc>:bd<CR>', {}),
-  vim.keymap.set({ 'n', 'i' }, '<C-z>', '', {}),
+  --vim.keymap.set({ 'n', 'i' }, '<C-z>', '', {}),
+  -- CopilotChat keymaps
+  vim.keymap.set({ 'n', 'v' }, '<leader>ce', ':CopilotChatExplain<CR>', {}),
+  vim.keymap.set({ 'n', 'v' }, '<leader>cr', ':CopilotChatReview<CR>', {}),
+  vim.keymap.set({ 'n', 'v' }, '<leader>cf', ':CopilotChatFix<CR>', {}),
+  vim.keymap.set({ 'n', 'v' }, '<leader>cd', ':CopilotChatDocs<CR>', {}),
+  vim.keymap.set({ 'n', 'v' }, '<leader>ct', ':CopilotChatTests<CR>', {}),
+  vim.keymap.set({ 'n', 'v', 'i' }, '<C-z>', ':CopilotChatToggle<CR>', {}),
 }
