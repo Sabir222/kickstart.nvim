70f74ef8e53e8c5a8b2b5c5cd0662330c0c46b72
sabir222 <mrsabir4@gmail.com>
2024-05-27 23:28:02 +0100

    update: updating the nvim cfg

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,7 +1,7 @@
 return {
   vim.keymap.set('n', '<;>', ':Neotree toggle<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', {}),
-  vim.keymap.set({ 'n', 'i' }, '<C-f>', ':copy.<CR>', {}),
+  vim.keymap.set({ 'n', 'i' }, '<C-f>', '<Esc>:copy.<CR>', {}),
   vim.keymap.set({ 'n', 'i' }, '<C-x>', '<Esc>:bd<CR>', {}),
   --vim.keymap.set({ 'n', 'i' }, '<C-z>', '', {}),
   -- CopilotChat keymaps
